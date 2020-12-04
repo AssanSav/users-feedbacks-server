ef21ab3953ecf9748498370d0a86ddd8ccc1aad6
AssanSav <assane.savadogo81@gmail.com>
2020-12-04 17:07:53 -0500

    first commit

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require("express")
+const mongoose = require("mongoose")
+const keys = require("./config/keys")
 const app = express()
 
 
-app.get("/", (req, res) => {
-  res.send("Route route")
-})
+mongoose.connect(keys.mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+
+require("./routes/googleAuth")(app)
+require("./services/passport")
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT)
