1d908a41e41ce0be35829637d2ce9ac28f2b98ea
AssanSav <assane.savadogo81@gmail.com>
2020-12-09 13:51:51 -0500

    Finished oAuth flow

--- config/prod.js
+++ config/prod.js
@@ -1,7 +1,7 @@
 module.exports = {
   mongoURI: process.env.MONGO_URI,
-  googleClientID: process.env.GOOGLE_CLIENT_ID,
   googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  googleClientID: process.env.GOOGLE_CLIENT_ID,
   cookieKey: process.env.COOKIE_KEY
 }
 
