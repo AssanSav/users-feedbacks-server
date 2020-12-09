4cab15c1bde6eba161f04f77c8d117fa2a70ac83
AssanSav <assane.savadogo81@gmail.com>
2020-12-08 21:24:20 -0500

    Finished oAuth flow

---
+++ config/dev.js
@@ -0,0 +1,21 @@
+module.exports = {
+  googleClientID: "658933104785-pa6ruh5jkmoqda0ot73dso1altoku0kt.apps.googleusercontent.com",
+  googleClientSecret: "dS-vyxomh9qeFod42fobg0q3",
+  googleCallbackURL: "/auth/google/callback",
+
+  facebookClientID: "417691489640564",
+  facebookClientSecret: "7897f0b0045ce849004dc6bd5c85193a",
+  facebookCallbackURL: "/auth/facebook/callback",
+
+  mongoURI: "mongodb+srv://Assane:Gourcy78050672,@feedbacks.qkfd3.mongodb.net/Assane?retryWrites=true&w=majority",
+  cookieKey: "asgsdhlkfjkgjlc!!!gdkfjdut8690765m,cx vf/lkgfijlmvs,nv,xmcvn.jvslkv"
+}
+
+// COOKIE_KEY assaerstdyucnfjgokhijndhysgatwqzxvbdh123546etry758osdov0cpvxlkxudcnvcj
+
+// GOOGLE_CLIENT_ID 515581919119 - 7nhvos0elde3vvfim0qppbiig3t41c17.apps.googleusercontent.com
+
+// GOOGLE_CLIENT_SECRET NmUY6F7HwL4dqOoMAYnKOxUC
+
+// MONGO_URI mongodb + srv://emaily:Gourcy78050672,@cluster0.liimu.mongodb.net/emaily?retryWrites=true&w=majority
+
