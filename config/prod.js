81105ffb82fee86a3de48686f00b75683d102ab5
AssanSav <assane.savadogo81@gmail.com>
2020-12-09 15:59:39 -0500

    Production URL added

--- config/prod.js
+++ config/prod.js
@@ -2,6 +2,7 @@ module.exports = {
   mongoURI: process.env.MONGO_URI,
   googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
   googleClientID: process.env.GOOGLE_CLIENT_ID,
+  googleCallbackURL: "/auth/google/callback",
   cookieKey: process.env.COOKIE_KEY
 }
 
