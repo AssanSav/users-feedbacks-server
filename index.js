687fd08a74139d6785ac5dcd8dafec6932a7bf12
AssanSav <assane.savadogo81@gmail.com>
2020-12-08 20:46:00 -0500

    Startup components, cookies and passport middleware

--- index.js
+++ index.js
@@ -1,17 +1,28 @@
 const express = require("express")
 const morgan = require("morgan")
 const mongoose = require("mongoose")
+const cookieSession = require("cookie-session")
+const passport = require("passport")
 const keys = require("./config/keys")
-const app = express()
 
+require("./models/User")
+require("./services/passport")
 
 mongoose.connect(keys.mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
 
+const app = express()
+
+app.use(cookieSession({
+  maxAge: 30 * 24 * 60 * 60 * 1000,
+  keys: [keys.cookieKey]
+}))
+
+app.use(passport.initialize())
+app.use(passport.session())
 
 app.use(morgan("tiny"))
-require("./models/User")
+require("./routes/facebookAuth")(app)
 require("./routes/googleAuth")(app)
-require("./services/passport")
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT)
