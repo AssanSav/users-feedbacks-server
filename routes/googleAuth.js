4bb4eae6971d7cafe5a51d6322e1e5d35918cc1b
AssanSav <assane.savadogo81@gmail.com>
2021-01-01 23:09:20 -0500

    Google auth routes handler

--- routes/googleAuth.js
+++ routes/googleAuth.js
@@ -1,24 +1,27 @@
-const passport = require("passport")
+const passport = require("passport");
 
 module.exports = (app) => {
   app.get(
     "/auth/google",
     passport.authenticate("google", {
-      scope: ["profile", "email"]
+      scope: ["profile", "email"],
     })
-  )
+  );
 
   app.get(
     "/auth/google/callback",
-    passport.authenticate("google")
-  )
+    passport.authenticate("google"),
+    (req, res) => {
+      res.redirect("/surveys");
+    }
+  );
 
   app.get("/api/current_user", (req, res) => {
-    res.send(req.user)
-  })
+    res.send(req.user);
+  });
 
   app.get("/api/logout", (req, res) => {
-    req.logout()
-    res.send(req.user)
-  })
-}
+    req.logout();
+    res.redirect("/");
+  });
+};
