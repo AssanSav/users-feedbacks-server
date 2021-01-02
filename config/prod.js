05f36727e2e4feaaf36143cdde73fcf093c1895b
AssanSav <assane.savadogo81@gmail.com>
2021-01-01 23:07:23 -0500

    Manages prod keys

--- config/prod.js
+++ config/prod.js
@@ -3,6 +3,7 @@ module.exports = {
   googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
   googleClientID: process.env.GOOGLE_CLIENT_ID,
   googleCallbackURL: "/auth/google/callback",
-  cookieKey: process.env.COOKIE_KEY
-}
-
+  cookieKey: process.env.COOKIE_KEY,
+  stripePublishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
+  stripeSecretKey: process.env.STRIPE_SECRET_KEY,
+};
