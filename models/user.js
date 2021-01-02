3399a55b493def9e87c751d207b6bb7fb5737134
AssanSav <assane.savadogo81@gmail.com>
2021-01-01 23:08:21 -0500

    User model schema

--- models/user.js
+++ models/user.js
@@ -1,10 +1,14 @@
-const mongoose = require("mongoose")
-const { Schema } = mongoose
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
 
 const userSchema = Schema({
   googleId: String,
   // facebookId: String,
   email: String,
-})
+  credits: {
+    type: Number,
+    default: 0,
+  },
+});
 
-mongoose.model("users", userSchema)
+mongoose.model("users", userSchema);
