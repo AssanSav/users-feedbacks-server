246e999cf10ca56dd5c3ea5fe8b3a2f8ceb321fa
AssanSav <assane.savadogo81@gmail.com>
2021-01-02 20:37:42 -0500

    Base URL added for redirects

--- routes/googleAuth.js
+++ routes/googleAuth.js
@@ -1,6 +1,7 @@
 const passport = require("passport");
+const keys = require("../config/keys");
 
-module.exports = (app) => {
+module.exports = function (app) {
   app.get(
     "/auth/google",
     passport.authenticate("google", {
@@ -12,16 +13,16 @@ module.exports = (app) => {
     "/auth/google/callback",
     passport.authenticate("google"),
     (req, res) => {
-      res.redirect("/surveys");
+      res.redirect(`${keys.baseURL}/surveys`);
     }
   );
 
-  app.get("/api/current_user", (req, res) => {
+  app.get("/api/users/current_user", (req, res) => {
     res.send(req.user);
   });
 
-  app.get("/api/logout", (req, res) => {
+  app.get("/api/users/logout", (req, res) => {
     req.logout();
-    res.redirect("/");
+    res.redirect(keys.baseURL);
   });
 };
