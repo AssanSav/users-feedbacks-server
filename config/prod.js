cd157bbe97e39b8769007ded66f162220062c435
AssanSav <assane.savadogo81@gmail.com>
2021-01-02 23:48:43 -0500

    Refact

--- config/prod.js
+++ config/prod.js
@@ -1,9 +1,9 @@
 module.exports = {
   baseURL: "https://users-feedback-app.herokuapp.com",
+  googleCallbackURL: "https://stormy-taiga-64394.herokuapp.com/auth/google/callback",
   mongoURI: process.env.MONGO_URI,
   googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
   googleClientID: process.env.GOOGLE_CLIENT_ID,
-  googleCallbackURL: "/auth/google/callback",
   cookieKey: process.env.COOKIE_KEY,
   stripePublishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
   stripeSecretKey: process.env.STRIPE_SECRET_KEY
