f2d256a5ff19d8801abbce713ce7d600bf8bb007
AssanSav <assane.savadogo81@gmail.com>
2021-01-02 21:12:31 -0500

    Refact

--- config/prod.js
+++ config/prod.js
@@ -1,5 +1,5 @@
 module.exports = {
-  mongoURI: process.env.MONGO_URI,
+  mongoURI: process.env.MONGO_URL,
   googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
   googleClientID: process.env.GOOGLE_CLIENT_ID,
   googleCallbackURL: "/auth/google/callback",
