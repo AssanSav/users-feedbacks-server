86ef9d3fc1933798eaa5731deed6fa7c821e7cc8
AssanSav <assane.savadogo81@gmail.com>
2021-01-05 10:55:27 -0500

    sendgrip prod env key variable

--- config/prod.js
+++ config/prod.js
@@ -6,6 +6,7 @@ module.exports = {
   googleClientID: process.env.GOOGLE_CLIENT_ID,
   cookieKey: process.env.COOKIE_KEY,
   stripePublishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
-  stripeSecretKey: process.env.STRIPE_SECRET_KEY
+  stripeSecretKey: process.env.STRIPE_SECRET_KEY,
+  sendGridKey: process.env.SEND_GRID_KEY
 };
 
